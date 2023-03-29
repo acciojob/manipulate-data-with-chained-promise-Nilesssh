0195fea9301e519358041b27442e8fa0da2abfd6
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-03-29 15:09:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const arr = [1,2,3,4];
  setTimeout(()=>setTimeout(()=>nice(arr),1000),3000);
 
 function nice(arr) {
-	document.getElementById("output").innerHTML = arr.filter(ele => ele%2!==0);
+	document.getElementById("output").innerHTML = arr.filter(ele => ele%2===0);
 	setTimeout(()=>mulWithTwo(arr),2000);
 }
 function mulWithTwo(arr) {
