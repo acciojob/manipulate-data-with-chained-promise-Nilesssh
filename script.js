2154991f23084e668c9d8bbd00cdfbe22bc060bc
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-03-29 15:07:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function nice(arr) {
 	setTimeout(()=>mulWithTwo(arr),2000);
 }
 function mulWithTwo(arr) {
-	const ans = arr.map(ele => ele%2===0 ? ele*2 : ele);
-	document.getElementById("output").innerHTML = ans;
+	const ans = arr.filter(ele => ele%2===0 );
+	const nel = ans.map(ele=>ele*2);
+	document.getElementById("output").innerHTML = nel;
 }
