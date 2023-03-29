307e112f4f9ff51fa59e360c5c209c08cd550ced
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-03-29 14:54:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-// //your JS code here. If required.
 const arr = [1,2,3,4];
 
-let first = setTimeout(findOdd(arr),3000);
+ setTimeout(()=>setTimeout(()=>nice(arr),1000),3000);
 
-function findOdd(arr) {
-	setTimeout(nice(arr),1000); 
-}
 function nice(arr) {
 	document.getElementById("output").innerHTML = arr.filter(ele => ele%2!==0);
-	hey(arr);
-}
-	function hey(arr) {
-	setTimeout(mulWithTwo(arr),2000);
+	setTimeout(()=>mulWithTwo(arr),2000);
 }
 function mulWithTwo(arr) {
-		const ans = arr.map(ele => ele*2);
+	const ans = arr.map(ele => ele*2);
 	document.getElementById("output").innerHTML = ans;
 }
-
-
